9fd83e65290dcf823fb627acb706a1c1ea429750
Konstantin Chervenko <kos.tch@yandex.ru>
2022-06-09 22:53:19 +0500

    создал основные элементы

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './scss/app.scss';
+import App from './App';
+
+const body = ReactDOM.createRoot(document.querySelector('body'));
+
+body.render(<App/>);
