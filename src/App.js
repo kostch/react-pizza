d8e84aec9f8e53440a74d065fd063ed511a42a08
Konstantin Chervenko <kos.tch@yandex.ru>
2022-08-07 11:44:39 +0500

    добавил redux начал считать количество пицц

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/filterSlice"
 import Header from './components/Header';
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -9,21 +11,37 @@ export const SearchContext = React.createContext();
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{searchValue, setSearchValue}}>
-        <Header/>
-        <div className="content">
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home/>}/>
-              <Route path="/cart" element={<Cart/>}/>
-              <Route path="*" element={<NotFound/>}/>
-            </Routes>
-          </div>
-        </div>
-      </SearchContext.Provider>
+
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+      {/*<SearchContext.Provider value={{searchValue, setSearchValue}}>*/}
+      {/*  <Header/>*/}
+      {/*  <div className="content">*/}
+      {/*    <div className="container">*/}
+      {/*      <Routes>*/}
+      {/*        <Route path="/" element={<Home/>}/>*/}
+      {/*        <Route path="/cart" element={<Cart/>}/>*/}
+      {/*        <Route path="*" element={<NotFound/>}/>*/}
+      {/*      </Routes>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</SearchContext.Provider>*/}
     </div>
   );
 }
